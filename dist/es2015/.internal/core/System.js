6b100438e42882752be7a186f513ae09430ecbb6
Pauan <pcxunlimited@gmail.com>
2018-05-08 07:22:27 -1000

    Version 4.0.0-beta.18

--- dist/es2015/.internal/core/System.js
+++ dist/es2015/.internal/core/System.js
@@ -382,7 +382,7 @@ var System = /** @class */ (function () {
      * @see {@link https://docs.npmjs.com/misc/semver}
      * @type {string}
      */
-    System.VERSION = "4.0.0-beta.17";
+    System.VERSION = "4.0.0-beta.18";
     return System;
 }());
 export { System };
