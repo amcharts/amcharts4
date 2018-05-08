7172df974fca4e5080799f51be45f805db62ff3a
Pauan <pcxunlimited@gmail.com>
2018-05-08 08:15:59 -1000

    Version 4.0.0-beta.20

--- dist/es2015/.internal/core/export/Export.js
+++ dist/es2015/.internal/core/export/Export.js
@@ -2194,7 +2194,7 @@ var Export = /** @class */ (function (_super) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, import(/* webpackChunkName: "../../fabric/fabric.js" */ "fabric")];
+                    case 0: return [4 /*yield*/, import(/* webpackChunkName: "fabric" */ "../../fabric/fabric")];
                     case 1: return [2 /*return*/, _a.sent()];
                 }
             });
