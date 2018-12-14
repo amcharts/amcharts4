0de439df9f7cfb62b2a9bb3e62e8ed71934dae3a
Achilles1515 <danereedhead@gmail.com>
2018-12-14 08:45:47 -0500

    Cleanup Popup "background" CSS properties

--- src/.internal/core/elements/PopupCSS.ts
+++ src/.internal/core/elements/PopupCSS.ts
@@ -49,8 +49,7 @@ export default function(prefix?: string): IDisposer {
 				/*"width": "100%",
 				"height": "100%",*/
 				"padding": "1em 2em",
-				"background": "rgb(255, 255, 255);",
-				"background-color": "rgba(255, 255, 255, 0.8)",
+				"background": "rgba(255, 255, 255, 0.8)",
 				"display": "inline-block",
 				"position": "absolute",
 				"top": "0",
@@ -66,9 +65,7 @@ export default function(prefix?: string): IDisposer {
 				"position": "absolute",
 				"top": "0.3em",
 				"right": "0.3em",
-				"background-color": "rgb(100, 100, 100)",
-				"background": "rgba(100, 100, 100, 0.1) url(data:image/svg+xml;charset=utf-8;base64,PHN2ZyBoZWlnaHQ9IjUxMiIgdmVyc2lvbj0iMSIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHdpZHRoPSI1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTQ0NS4yIDEwOS4ybC00Mi40LTQyLjRMMjU2IDIxMy42IDEwOS4yIDY2LjhsLTQyLjQgNDIuNEwyMTMuNiAyNTYgNjYuOCA0MDIuOGw0Mi40IDQyLjRMMjU2IDI5OC40bDE0Ni44IDE0Ni44IDQyLjQtNDIuNEwyOTguNCAyNTYiLz48L3N2Zz4=) no-repeat center",
-				"background-size": "80%",
+				"background": "rgba(100, 100, 100, 0.1) url(data:image/svg+xml;charset=utf-8;base64,PHN2ZyBoZWlnaHQ9IjUxMiIgdmVyc2lvbj0iMSIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHdpZHRoPSI1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTQ0NS4yIDEwOS4ybC00Mi40LTQyLjRMMjU2IDIxMy42IDEwOS4yIDY2LjhsLTQyLjQgNDIuNEwyMTMuNiAyNTYgNjYuOCA0MDIuOGw0Mi40IDQyLjRMMjU2IDI5OC40bDE0Ni44IDE0Ni44IDQyLjQtNDIuNEwyOTguNCAyNTYiLz48L3N2Zz4=) no-repeat center/80%",
 				"width": "1.2em",
 				"height": "1.2em",
 				"cursor": "pointer"
